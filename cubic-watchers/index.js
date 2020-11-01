e77a0b7b305b1051e561864233af3ccef4820f63
Yaroslav Ryzhkov <666anastyalena@mail.ru>
2020-11-01 21:54:36 +0300

    feat: implement different forms

--- cubic-watchers/index.js
+++ cubic-watchers/index.js
@@ -1,11 +1,20 @@
+const body = document.querySelector("body");
 const colorBtn = document.querySelector(".color-button");
 const discoBtn = document.querySelector(".disco-button");
-const body = document.querySelector("body");
+const figureBtn = document.querySelector(".change-button");
 const watchers = document.querySelectorAll(".watcher");
-
 
 const bodyBGC = ["", "#1B1464", "#6F1E51", "#5758BB", "#A3CB38", "#F79F1F", "#1289A7", "#f19066", "#c0392b", "#d35400", "#27ae60", "#1e272e", "#808e9b", "#3c40c6", "#ffa801", "#0fbcf9", "#2c2c54", "#cc8e35", "#b33939", "#227093", "#218c74", "#2C3A47", "#182C61", "#82589F"];
 const watcherBGC = ["", "#0652DD", "#833471", "#9980FA", "#C4E538", "#FFC312", "#12CBC4", "#f3a683", "#e74c3c", "#e67e22", "#2ecc71", "#485460", "#d2dae2", "#575fcf", "#ffc048", "#4bcffa", "#40407a", "#ffb142", "#ff5252", "#34ace0", "#33d9b2", "#CAD3C8", "#3B3B98", "#D6A2E8"];
+const forms = [
+  "polygon(50% 0%, 0% 100%, 100% 100%, 100% 100%, 100% 100%, 100% 100%, 100% 100%, 100% 100%, 100% 100%, 100% 100%, 100% 100%, 100% 100%)",
+  "polygon(0 35%, 25% 35%, 25% 10%, 50% 10%, 50% 35%, 100% 35%, 100% 65%, 50% 65%, 50% 90%, 25% 90%, 25% 65%, 0 65%)",
+  "polygon(20% 0%, 100% 20%, 100% 75%, 20% 100%, 0% 80%, 0% 20%, 0% 20%, 0% 20%, 0% 20%, 0% 20%, 0% 20%, 0% 20%)",
+  "polygon(20% 0%, 100% 50%, 20% 100%, 0% 80%, 0% 20%,  0% 20%, 0% 20%, 0% 20%, 0% 20%, 0% 20%, 0% 20%, 0% 20%)",
+  "polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%, 0% 50%, 0% 50%, 0% 50%, 0% 50%, 0% 50%, 0% 50%, 0% 50%, 0% 50%)",
+  "polygon(0% 20%, 60% 20%, 60% 0%, 100% 50%, 60% 100%, 60% 80%, 0% 80%, 0% 80%, 0% 80%, 0% 80%, 0% 80%, 0% 80%)",
+  "polygon(0 35%, 35% 35%, 35% 0%, 50% 0, 65% 35%, 100% 50%, 65% 65%, 50% 100%, 35% 100%, 35% 65%, 0 65%, 0% 65%)"
+]
 
 let isDisco = false;
 let colorNumber = Math.floor(Math.random() * bodyBGC.length) + 1;
@@ -31,6 +40,7 @@ colorBtn.addEventListener("click", () => {
   }, 500)
   changeBGC();
 });
+
 discoBtn.addEventListener("click", () => {
   isDisco = !isDisco;
   changeBGC();
@@ -40,6 +50,25 @@ discoBtn.addEventListener("click", () => {
     discoBtn.style.backgroundColor = "transparent";
   }
 });
+
+let formsCounter = Math.floor(Math.random() * forms.length) + 1;
+
+figureBtn.addEventListener("click", () => {
+  if(formsCounter === forms.length){
+    formsCounter = 0;
+  }
+
+  figureBtn.style.backgroundColor = "black";
+  setTimeout(() => {
+    figureBtn.style.backgroundColor = "transparent";
+  }, 500);
+
+  watchers.forEach((e) => {
+    e.style.clipPath = forms[formsCounter];
+  })
+
+  formsCounter += 1;
+})
 
 document.addEventListener("mousemove", (e) => {
   const sqrs = document.querySelectorAll(".watcher");
