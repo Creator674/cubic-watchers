cc4969b5c5090a9fe184f2f4845a7549a74d8a46
Yaroslav Ryzhkov <666anastyalena@mail.ru>
2020-11-01 13:42:44 +0300

    feat: add different colors

--- cubic-watchers/index.js
+++ cubic-watchers/index.js
@@ -1,3 +1,22 @@
+const colorBtn = document.querySelector(".color-button");
+const body = document.querySelector("body");
+const watchers = document.querySelectorAll(".watcher");
+
+
+const bodyBGC = ["", "#1B1464", "#6F1E51", "#5758BB", "#A3CB38", "#F79F1F", "#1289A7", "#f19066", "#c0392b", "#d35400", "#27ae60", "#1e272e", "#808e9b", "#3c40c6", "#ffa801", "#0fbcf9", "#2c2c54", "#cc8e35", "#b33939", "#227093", "#218c74", "#2C3A47", "#182C61", "#82589F"];
+const watcherBGC = ["", "#0652DD", "#833471", "#9980FA", "#C4E538", "#FFC312", "#12CBC4", "#f3a683", "#e74c3c", "#e67e22", "#2ecc71", "#485460", "#d2dae2", "#575fcf", "#ffc048", "#4bcffa", "#40407a", "#ffb142", "#ff5252", "#34ace0", "#33d9b2", "#CAD3C8", "#3B3B98", "#D6A2E8"];
+
+function changeBGC() {
+  const colorNumber = Math.floor(Math.random() * bodyBGC.length) + 1;
+  body.style.backgroundColor = bodyBGC[colorNumber];
+  watchers.forEach(e => e.style.backgroundColor = watcherBGC[colorNumber]);
+
+  setTimeout(() => changeBGC(), 1500)
+}
+
+colorBtn.addEventListener("click", changeBGC);
+
+
 document.addEventListener("mousemove", (e) => {
   const sqrs = document.querySelectorAll(".watcher");
 
