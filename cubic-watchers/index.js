f4a952bfafd7264ac4d6db641cd78debc8ae75e8
Yaroslav Ryzhkov <666anastyalena@mail.ru>
2020-11-12 22:48:42 +0300

    feat: remove shadow in mobile version

--- cubic-watchers/index.js
+++ cubic-watchers/index.js
@@ -4,6 +4,8 @@ const discoBtn = document.querySelector(".disco-button");
 const figureBtn = document.querySelector(".change-button");
 const shadowBtn = document.querySelector(".shadow-button");
 const watchers = document.querySelectorAll(".watcher");
+const pcIcon = document.querySelector(".shadow-button--pc");
+const shadowBtnIcon = document.querySelector(".shadow-button--icon");
 
 const bodyBGC = ["#546de5", "#1B1464", "#6F1E51", "#5758BB", "#A3CB38", "#F79F1F", "#1289A7", "#f19066", "#c0392b", "#d35400", "#27ae60", "#1e272e", "#808e9b", "#3c40c6", "#ffa801", "#0fbcf9", "#2c2c54", "#cc8e35", "#b33939", "#227093", "#218c74", "#2C3A47", "#182C61", "#82589F"];
 const watcherBGC = ["#778beb", "#0652DD", "#833471", "#9980FA", "#C4E538", "#FFC312", "#12CBC4", "#f3a683", "#e74c3c", "#e67e22", "#2ecc71", "#485460", "#d2dae2", "#575fcf", "#ffc048", "#4bcffa", "#40407a", "#ffb142", "#ff5252", "#34ace0", "#33d9b2", "#CAD3C8", "#3B3B98", "#D6A2E8"];
@@ -77,20 +79,18 @@ figureBtn.addEventListener("click", () => {
   formsCounter += 1;
 })
 
-
-
 let isColoring = false;
 
-shadowBtn.addEventListener("click", () => {
+function shadowHandler () {
   isColoring =! isColoring;
-
   if(isColoring) {
     shadowBtn.style.backgroundColor = "black";
   } else {
     shadowBtn.style.backgroundColor = "transparent";
   }
-})
+}
 
+shadowBtn.addEventListener("click", shadowHandler);
 
 document.addEventListener("mousemove", (e) => {
   const mouseX = e.pageX;
@@ -105,7 +105,7 @@ document.addEventListener("mousemove", (e) => {
         e.target.style.backgroundColor = watcherBGC[colorNumber-1];
         e.target.style.transition = "background-color 1s ease-in-out, clip-path 1s ease-in-out";
       }, 1500)
-  }
+    }
   }
 
   watchers.forEach((sqr) => {
@@ -125,8 +125,8 @@ document.addEventListener("mousemove", (e) => {
 
 
 document.addEventListener("touchmove", (e) => {
-  const mouseX = e.targetTouches[0].clientX;
-  const mouseY = e.targetTouches[0].clientY;
+  const mouseX = e.touches[0].clientX;
+  const mouseY = e.touches[0].clientY;
 
   if(isColoring){
     if(e.target.className === "watcher"){
@@ -151,6 +151,20 @@ document.addEventListener("touchmove", (e) => {
 
     const angle = (radians * 180) / Math.PI;
     sqr.style.transform = `rotate(${angle}deg)`;
-
   });
 }, false);
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  pcIcon.style.display = "inline-block";
+  shadowBtnIcon.style.display = "none";
+  shadowBtn.removeEventListener('click', shadowHandler);
+  shadowBtn.addEventListener("click", () => {
+    pcIcon.classList.add('pc--animation');
+    setTimeout(() => {
+      pcIcon.classList.remove('pc--animation');
+    }, 1000);
+  })
+  } else {
+  pcIcon.style.display = "none";
+  shadowBtnIcon.style.display = "inline-block";
+}
